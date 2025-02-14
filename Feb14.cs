4f52218198a011c1e312199658d2df18b059a39a
Hamzah9729 <hamzahR756@stemcollegiate.ca>
2025-02-14 10:15:59 -0700

    Create Feb14.cs

---
+++ Feb14.cs
@@ -0,0 +1,7 @@
+//Journal for Feb 14,2025
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+// I learned that variables can have many different names but you have to be very specific when you type the variable's name out. Variables names are case sensitive.
