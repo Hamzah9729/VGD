e05790f124ed55fa89ba97c85fecb4e3e25daa69
Hamzah9729 <hamzahR756@stemcollegiate.ca>
2025-02-24 13:54:29 -0700

    Create Feb24.cs

---
+++ Feb24.cs
@@ -0,0 +1,7 @@
+var message = "Hello world!";
+message = 10.703m;
+var message;
+// I learned today that the var keyword is very common in coding 
+var message = "Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.";
+Console.Write()
+  // I learned how to write a custom message using the var keyword
