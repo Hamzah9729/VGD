1a0762047a36a8f3214ed0aad61c709473fcc85e
Hamzah9729 <hamzahR756@stemcollegiate.ca>
2025-03-07 10:41:18 -0700

    Create Mar07.cs

---
+++ Mar07.cs
@@ -0,0 +1,23 @@
+//This is Unit 3 code
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+//Unit 4
+string firstName = "Bob";
+Console.WriteLine(firstName);
+// Unit 5
+var message = "Hello World!";
+//Unit 6
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
