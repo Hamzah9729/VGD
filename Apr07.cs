63c205db1543ed9681d18c8efc579d29001b2f0e
Hamzah9729 <hamzahR756@stemcollegiate.ca>
2025-04-07 13:47:23 -0600

    Create Apr07.cs

---
+++ Apr07.cs
@@ -0,0 +1,6 @@
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+//Module 5 Unit 1 and 2
+//I learned that I can do basic addition in the .NET editor
