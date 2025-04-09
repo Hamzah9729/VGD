e058dcf27e63a3c8dc594334b5d161c194400a1f
Hamzah9729 <hamzahR756@stemcollegiate.ca>
2025-04-09 08:46:57 -0600

    Create Apr09.cs

---
+++ Apr09.cs
@@ -0,0 +1,14 @@
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+
+//Module 5 Unit 3 and 4
+//I learned that the .NET editor uses BEDMAS to solve equations
